046365250885a31da956d145450f2056afeecd5a
David Hong <gdavid0510@gmail.com>
2022-10-19 22:10:38 +0900

    service setup script bugfix

--- setup/sysmon-oled.sh
+++ setup/sysmon-oled.sh
@@ -3,9 +3,16 @@
 echo -e "\n\033[103m\033[30m\033[1m  ODROID H2 PROXMOX i2c-oled monitoring service  \033[0m"
 
 
-# echo -e "\n\033[93m\033[1mFinding I2C port\033[0m"
+# echo -e "\n\033[93m\033[1mSettings\033[0m"
+
 # TODO: automatically detect I2C device no. and port
 I2C_PORT=2
+
+# TODO: set orientation.
+ORIENTATION=0	# 0 deg
+#ORIENTATION=1	# 90 deg clockwise
+#ORIENTATION=2	# 180 deg
+#ORIENTATION=3	# 270 deg clockwise
 
 
 echo -e "\n\033[93m\033[1mInstalling prerequisities\033[0m"
@@ -18,10 +25,12 @@ pip install -U luma.core
 
 echo -e "\n\033[93m\033[1mInstalling service\033[0m"
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p /opt/sysmon-oled
-cp -r ../sysmon-oled /opt/sysmon-oled
-cp ./sysmon-oled.service /etc/systemd/system/sysmon-oled.service
+cp -r ${SCRIPT_DIR}/../sysmon-oled /opt/sysmon-oled
+cp ${SCRIPT_DIR}/sysmon-oled.service /etc/systemd/system/sysmon-oled.service
 sed -i 's/%I2CPORT%/'${I2C_PORT}'/g' /etc/systemd/system/sysmon-oled.service
+sed -i 's/%ORIENTATION%/'${ORIENTATION}'/g' /etc/systemd/system/sysmon-oled.service
 systemctl daemon-reload
 systemctl enable sysmon-oled.service
 
